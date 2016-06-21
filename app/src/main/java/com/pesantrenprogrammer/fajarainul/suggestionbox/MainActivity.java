c05cecb3b43673d17e7b2551954ae602c8941396
fajarainul <fajarainul14@gmail.com>
2016-06-21 18:03:28 +0700

    initial commit

---
+++ app/src/main/java/com/pesantrenprogrammer/fajarainul/suggestionbox/MainActivity.java
@@ -0,0 +1,27 @@
+package com.pesantrenprogrammer.fajarainul.suggestionbox;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_president;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_president = (Button)findViewById(R.id.button_leader);
+
+        btn_president.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PresidentActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
